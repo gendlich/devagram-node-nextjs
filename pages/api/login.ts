2a65903afcda033422d9b0ebdda575c92d13601d
gendlich <g.endlich.g@gmail.com>
2022-02-21 18:47:30 -0300

    pre aula 21/02

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,37 +1,35 @@
-import type {NextApiRequest,NextApiResponse} from  'next';
+import type {NextApiRequest, NextApiResponse} from 'next';
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
-import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
-import type { LoginResposta } from '../../types/LoginResposta';
-import { UsuarioModel } from "../../models/UsuarioModel";
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
+import type {LoginResposta} from '../../types/LoginResposta';
 import md5 from 'md5';
+import { UsuarioModel } from '../../models/UsuarioModel';
 import jwt from 'jsonwebtoken';
 
 const endpointLogin = async (
-    req: NextApiRequest,
-    res: NextApiResponse<RespostaPadraoMsg | LoginResposta>
+    req : NextApiRequest,
+    res : NextApiResponse<RespostaPadraoMsg | LoginResposta>
 ) => {
 
-    const { MINHA_CHAVE_JWT} = process.env;
+    const {MINHA_CHAVE_JWT} = process.env;
     if(!MINHA_CHAVE_JWT){
-        return res.status(500).json({erro: 'ENV JWT nao informada'})
+        return res.status(500).json({erro : 'ENV Jwt nao informada'});
     }
 
     if(req.method === 'POST'){
-        const {login, senha} = req.body
+        const {login, senha} = req.body;
 
-        const usuariosEncontrados = await UsuarioModel.find({email: login, senha: md5(senha)});
+        const usuariosEncontrados = await UsuarioModel.find({email : login, senha : md5(senha)});
         if(usuariosEncontrados && usuariosEncontrados.length > 0){
-            const usuarioEncontrado = usuariosEncontrados[0];
+            const usuarioEncotrado = usuariosEncontrados[0];
 
-            const token = jwt.sign({_id: usuarioEncontrado._id}, MINHA_CHAVE_JWT);
-
+            const token = jwt.sign({_id : usuarioEncotrado._id}, MINHA_CHAVE_JWT);
             return res.status(200).json({
-                nome: usuarioEncontrado.nome,
-                email: usuarioEncontrado.email,
-                token
-            });
+                nome : usuarioEncotrado.nome, 
+                email : usuarioEncotrado.email, 
+                token});
         }
-        return res.status(400).json({erro : 'Usuario ou senha nao encontrados'});
+        return res.status(400).json({erro : 'Usuario ou senha nao encontrado'});
     }
     return res.status(405).json({erro : 'Metodo informado nao e valido'});
 }
